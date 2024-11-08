0caf6f3c25eeb07f085290e81acd195d64c80015
Alfred <arista21dominguez@gmail.com>
2024-11-07 20:15:14 -0600

    Fondo

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -1,13 +1,48 @@
 
 package gui;
 
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 /**
  *
  * @author Alfred
  */
-public class Ventana extends JFrame
+public class Ventana extends JFrame implements Runnable
 {
+    private JLayeredPane panelPrincipal;
+
+    public Ventana()
+    {
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+        setSize(670, 230);
+        initComponents();
+    }
+
+    private void initComponents()
+    {
+        panelPrincipal = new JLayeredPane();
+        panelPrincipal.setPreferredSize(new Dimension(670, 230));
+        setContentPane(panelPrincipal);
+        panelPrincipal.setLayout(null);
+
+        ImageIcon n1 = new ImageIcon("src/gui/src/banda.PNG");
+        JLabel lblFondo1 = new JLabel(n1);
+        lblFondo1.setBounds(0, 0, n1.getIconWidth(), n1.getIconHeight());
+        
+        panelPrincipal.add(lblFondo1, Integer.valueOf(0));
+    }
+
+    @Override
+    public void run()
+    {
+        
+    }
     
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 
 package main;
 
+import gui.Ventana;
+
 /**
  *
  * @author Alfred
@@ -9,6 +11,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Aqui va mi practica ... xD");
+        new Thread(new Ventana()).start();
     }
 }
